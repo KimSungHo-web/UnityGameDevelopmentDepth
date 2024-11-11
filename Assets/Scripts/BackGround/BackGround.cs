ee921382401ed54ab72eb934ec30c530a202700d
KimSungHo-web <epdlam20@gmail.com>
2024-11-11 17:13:45 +0900

    feat : BackGround

---
+++ Assets/Scripts/BackGround/BackGround.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class BackGround : MonoBehaviour
+{
+    [Header ("Clouds")]
+    [SerializeField] private GameObject _clouds;
+    [SerializeField] private float _cloudsSpeed; // 구름의 속도
+    [SerializeField] private float _cloudsEnd; // 구름의 끝나는 지점
+    private Vector3 _cloudsStartPosition; // 구름의 시작 위치
+   
+    private void Awake()
+    {
+        _clouds = transform.Find("Clouds").gameObject;
+
+        _cloudsStartPosition = _clouds .transform.position;
+    }
+
+    private void Update()
+    {
+        _clouds.transform.Translate(Vector3.right * _cloudsSpeed * Time.deltaTime);
+
+        if (_clouds.transform.position.x >= _cloudsEnd) 
+        {
+            ResetCloudPosition();
+        }
+
+    }
+
+    private void ResetCloudPosition()
+    {
+        _clouds.transform.position = _cloudsStartPosition;
+    }
+}
